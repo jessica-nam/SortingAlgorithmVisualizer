e148f91eae540c723de4d669078fce8ca5160cd9
Jessica Nam <jessicanam@nevada.unr.edu>
2022-08-15 14:26:13 -0700

    Implemented Bubble Sort

--- initializer.py
+++ initializer.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 
 class DrawInformation: #global values, better to use a class instead of global variables - less flexibility from global scope
@@ -37,7 +38,7 @@ class DrawInformation: #global values, better to use a class instead of global v
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
 
         #height depends on range of values
-        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
 
         #start drawing blocks from x coord considering side pad space
         self.start_x = self.SIDE_PAD // 2
@@ -55,8 +56,13 @@ def draw(draw_info):
     pygame.display.update()
 
 
-def draw_list(draw_info):
+def draw_list(draw_info, color_positions = {}, clear_background = False):
     lst = draw_info.lst
+
+    if clear_background:
+        clear_rect = (draw_info.SIDE_PAD // 2, draw_info.TOP_PAD, draw_info.width - draw_info.SIDE_PAD, draw_info.height - draw_info.TOP_PAD)
+        #draw rectangle
+        pygame.draw.rect(draw_info.window, draw_info.BACKGROUND, clear_rect)
     for i, val in enumerate(lst): #enumerate gives index and value of every element in list
         #x, y from top left hand corner
         x = draw_info.start_x + i * draw_info.block_width
@@ -64,8 +70,14 @@ def draw_list(draw_info):
         y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
 
         color = draw_info.GREYS[i % 3] 
-        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
 
+        if i in color_positions:
+            color = color_positions[i]
+
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
+    
+    if clear_background:
+        pygame.display.update()
 
 def generate_starting_list(n, min_val, max_val):
     """Randomly generates list to be sorted"""
@@ -73,6 +85,21 @@ def generate_starting_list(n, min_val, max_val):
     for _ in range(n):
         val = random.randint(min_val, max_val)
         lst.append(val)
+    return lst
+
+def bubble_sort(draw_info, ascending = True):
+    """Implements Bubble Sort algorithm"""
+    lst = draw_info.lst
+
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - 1 -i):
+            num1 = lst[j]
+            num2 = lst[j+1]
+
+            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+                lst[j], lst[j + 1] = lst [j + 1], lst[j] #swap
+                draw_list(draw_info, {j: draw_info.GREEN, j + 1: draw_info.RED}, True)
+                yield True #yield makes function a generator
     return lst
 
 
@@ -90,8 +117,20 @@ def main():
     sorting = False
     ascending = True
 
+    sorting_algorithm = bubble_sort
+    sorting_algorithm_name = "Bubble Sort"
+    sorting_algorithm_generator = None
+
     while run:
         clock.tick(60) #max number of times this loop can run per second
+
+        if sorting: #if currently sorting
+            try: #try to call next method
+                next(sorting_algorithm_generator)
+            except StopIteration: #generator is done
+                sorting = False
+        else:
+            draw(draw_info)
 
         draw(draw_info)
         pygame.display.update() #render display
@@ -110,6 +149,7 @@ def main():
             
             elif event.key == pygame.K_SPACE and sorting == False:
                 sorting = True
+                sorting_algorithm_generator = sorting_algorithm(draw_info, ascending)
 
             elif event.key == pygame.K_a and not sorting:
                 ascending = True
